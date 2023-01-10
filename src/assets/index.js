58ce111968758d5c44e97c8dedbc4d1ecd0b18bf
saharghoniem <saharghoniem@icloud.com>
2023-01-11 01:41:09 +0200

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import airbnb from "./airbnb.png";
+import bill from "./asset.jpg";
+import binance from "./binance.png";
+import card from "./card.jpg";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import bank from "./bank.jpg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./person2.jpg";
+import people02 from "./person3.jpg";
+import people03 from "./person4.jpg";
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  bank,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
